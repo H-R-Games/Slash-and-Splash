b491911dd94994e2d0b726d1968ffbf62ad99398
Alejandro Font <fontalejandro0@gmail.com>
2022-12-01 16:12:44 +0100

    Update FloorManeger.cs

--- Assets/_Scripts/Managers/FloorManeger.cs
+++ Assets/_Scripts/Managers/FloorManeger.cs
@@ -6,11 +6,12 @@ using UnityEngine;
 public class FloorManeger : MonoBehaviour
 {
     [Header("Floor Settings")]
-    public GameObject floorPrefab;
-    public GameObject floorDeadPrefab;
-    [Range(0, 100)]public int floorCount = 120;
+    [SerializeField] private GameObject _floorPrefab;
+    [SerializeField] private GameObject _floorDeadPrefab;
+    [SerializeField][Range(0, 150)] private int _floorCount = 120;
     private float _distance = 15;
-    [SerializeField]private List<GameObject> _floors;
+    
+    List<GameObject> _floors = new List<GameObject>();
 
     private GameManager _gm;
 
@@ -19,7 +20,6 @@ public class FloorManeger : MonoBehaviour
         _gm = FindObjectOfType<GameManager>();
         _gm.RestartGame += SpawnFloor;
 
-        _floors = new List<GameObject>();
         SpawnFloor();
         DeadZone();
     }
@@ -36,14 +36,14 @@ public class FloorManeger : MonoBehaviour
     {
         StartFloor();
         
-        for (int i = 0; i < floorCount; i++)
+        for (int i = 0; i < _floorCount; i++)
         {
             _distance += Random.Range(20, 30);
             int _scaleX = Mathf.Abs(Random.Range(5, 10));
             float x = new Vector3(Random.Range(-_distance, _distance), 0f, 0f).x;
             Vector2 v = new Vector2(x, -5);
 
-            GameObject _floor = Instantiate(floorPrefab, v, transform.rotation);
+            GameObject _floor = Instantiate(_floorPrefab, v, transform.rotation);
             _floor.transform.localScale = new Vector3(_scaleX, 1, 1);
             _floors.Add(_floor);
         }
@@ -54,7 +54,7 @@ public class FloorManeger : MonoBehaviour
     /// </summary>
     private void StartFloor()
     {
-        GameObject floor = Instantiate(floorPrefab, new Vector3(0, -5, 0), Quaternion.identity);
+        GameObject floor = Instantiate(_floorPrefab, new Vector3(0, -5, 0), Quaternion.identity);
         int _scaleX = Mathf.Abs(Random.Range(5, 10));
         floor.transform.localScale = new Vector3(_scaleX, 1, 1);
         _floors.Add(floor);
@@ -65,7 +65,7 @@ public class FloorManeger : MonoBehaviour
     /// </summary>
     private void DeadZone()
     {
-        GameObject _DeadZone = Instantiate(floorDeadPrefab, new Vector3(0, -30, 0), Quaternion.identity);
+        GameObject _DeadZone = Instantiate(_floorDeadPrefab, new Vector3(0, -30, 0), Quaternion.identity);
         _DeadZone.transform.localScale = new Vector3(100000, 50, 1);
     }
 
